09d978a4e8da9dd3c079a8e238da5f96b47c67a1
Lawrence Chu <DOLLARS1912@192-168-1-12.tpgi.com.au>
2017-09-05 21:50:10 +1000

    add new work week8

---
+++ 374sprint1_profilePages/popup login/js/index.js
@@ -0,0 +1,52 @@
+﻿function deleteLogin(){
+	var del=document.getElementById("login_box");
+	var bg_filter=document.getElementById("bg_filter");
+	bg_filter.style.display="none";
+	del.style.display="none";
+}
+function showBox(){
+	var show=document.getElementById("login_box");
+	var bg_filter=document.getElementById("bg_filter");
+	show.style.display="block";
+	bg_filter.style.display="block";
+}
+
+var tmpHtml="";
+
+var obj={
+
+	login:$(".login"),
+
+	init:function(){
+		$(".wapper").click(function(){
+			obj.sweep();
+		});
+	},
+
+	sweep:function(){
+		tmpHtml=obj.login.html();
+		obj.login.empty();
+		obj.barcode();
+	},
+
+	barcode:function(){
+		var text=$("<div>");
+		text.addClass("text");
+		text.html("Register");
+		obj.login.append(text);
+
+		var img=$("<img src='img/sweep01.jpg' />");
+		img.addClass("img");
+		obj.login.append(img);
+
+		var back=$("<div onclick='obj.reback()'>");
+		back.addClass("back");
+		back.html("Back to login!!! hahaha I am zhe wang !!");
+		obj.login.append(back);
+
+	},
+	reback:function(){
+		obj.login.html(tmpHtml);
+	}
+}
+obj.init();
